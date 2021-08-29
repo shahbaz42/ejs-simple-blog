c90ec62b403d9c93f9d67670aaf10b83fb8cf5e0
shahbaz ali <shahbaz_ali@outlook.in>
2021-08-30 00:56:24 +0530

    display posts on home

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const posts = [];
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -19,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { content: homeStartingContent });
+  res.render("home", { content: homeStartingContent, posts : posts });
 });
 
 app.post("/compose", function (req, res) {
@@ -27,7 +29,8 @@ app.post("/compose", function (req, res) {
     title: req.body.postTitle,
     post: req.body.postBody
   };
-  console.log(post);
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.get("/about", function (req, res) {
